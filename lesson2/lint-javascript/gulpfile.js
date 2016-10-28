de05ccfc6fc042297f115053e30057db536b2118
ics-nohara <nozomi.nohara@ics-web.jp>
2016-10-28 15:10:28 +0900

    lesson2 : lintにコメントを追加

--- lesson2/lint-javascript/gulpfile.js
+++ lesson2/lint-javascript/gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require("gulp");
 var eslint = require("gulp-eslint");
 
+/*
+ * lintを使ってJavaScriptファイルのチェックを行う
+ */
+
 gulp.task("lint-javascript", function () {
   return gulp.src(["src/**.js"])
     .pipe(eslint(".eslintrc"))
