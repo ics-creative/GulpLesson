952e0d605ca20c1b4d1b4269f880172a52dff097
tonkotsuboy <t.kano.624@gmail.com>
2016-11-09 04:20:23 +0900

    参考用gulpfile.jsをアップ

--- lesson2/gulpfile-1.js
+++ lesson2/gulpfile-1.js
@@ -1,1 +1,15 @@
+var gulp = require("gulp");
+var browserSync = require("browser-sync");
 
+gulp.task("default", function () {
+  browserSync.init({
+    online: true,
+    server: {
+      baseDir: "./src"
+    }
+  });
+
+  gulp.watch("src/**", function () {
+    browserSync.reload();
+  });
+});
