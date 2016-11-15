6abd315138e2d935cb909b19c890a0f96d46dae4
tonkotsuboy <t.kano.624@gmail.com>
2016-11-16 04:50:32 +0900

    各タスク定義

--- lesson3/project/gulp/tasks/concat.js
+++ lesson3/project/gulp/tasks/concat.js
@@ -1,8 +1,12 @@
+/**
+ * 1-2. JavaScriptファイルの結合
+ */
+
 var gulp = require("gulp");
 var concat = require("gulp-concat");
 
-gulp.task("concat", function() {
-  return gulp.src("src/js/*.js")
+gulp.task("concat", function () {
+  gulp.src("src/js/*.js")
     .pipe(concat("script.js"))
     .pipe(gulp.dest("dist/js"));
 });
--- lesson3/project/gulp/tasks/imagemin.js
+++ lesson3/project/gulp/tasks/imagemin.js
@@ -1,9 +1,12 @@
+/**
+ * 1-3. PNG画像のファイルサイズ削減
+ */
+
 var gulp = require("gulp");
 var imageMin = require("gulp-imagemin");
-// pngの圧縮
 var pngquant = require("imagemin-pngquant");
 
-gulp.task("imagemin", function() {
+gulp.task("imagemin", function () {
   gulp.src("src/images/*")
     .pipe(imageMin(
       [pngquant({quality: "70", speed: 1})]
